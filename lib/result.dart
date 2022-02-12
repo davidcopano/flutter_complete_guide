07a866e4b548779a45367049568ea6835ffa635b
David Copano <davidcopano96@gmail.com>
2022-02-12 12:42:12 +0100

    Following the course

--- lib/result.dart
+++ lib/result.dart
@@ -22,10 +22,20 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: () {
+              print('Reset clicked!');
+            },
+            child: Text('Reset'),
+          ),
+        ],
       ),
     );
   }
